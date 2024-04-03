5ea2364858a41d0982fd8bba76a45ba0eaaf888a
gerard <12766134+scripting-drafts@users.noreply.github.com>
2024-04-04 00:53:52 +0200

    bugfixes and improvements

--- wifi_toaster.py
+++ wifi_toaster.py
@@ -1,51 +1,166 @@
 import pywifi
+import pywifi.const as const
 import time
+import keyring
+import requests
+import logging
+import time
+import creds
+import subprocess
+import random
 
 class Geored_Wifi_Server:
     def __init__(self):
         self.wifi = pywifi.PyWiFi()
+        logging.basicConfig(level=logging.INFO)
 
     def get_network_interfaces(self):
-        'Interfaces (ifs) is a list of objects with the attribute ifs.name'
-        self.ifaces = self.wifi.interfaces() 
-        ifs = self.ifaces
+        '''
+        ifaces is a list of objects with the attribute ifaces.name
+        We assume ifaces will never be empty
+        '''
+        self.ifaces = self.wifi.interfaces()
+        self.iface_a = self.ifaces[0]
 
-        for u, w in enumerate(ifs):
+        for u, w in enumerate(self.ifaces):
             print(f'iface {u}:', w.name())
-
-        return ifs
 
     def connect_available_iface(self):
         return
-    
-    def connect_available_network(self):
-        return
-    
+
+    def connect_ap(self, iface, ap):
+        iface.remove_all_network_profiles()
+
+        profile = pywifi.Profile()
+        profile.ssid = ap
+        profile.auth = const.AUTH_ALG_OPEN
+        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        profile.cipher = const.CIPHER_TYPE_CCMP
+        pwd = keyring.get_password('system', ap)
+        profile.key = pwd
+        
+        iface.connect(profile)
+        time.sleep(5)
+        assert iface.status() == const.IFACE_CONNECTED
+
+    def disable_software_radio(self, iface):
+        iface.disconnect()
+        time.sleep(.7)
+        assert iface.status() in\
+            [const.IFACE_DISCONNECTED, const.IFACE_INACTIVE]
+        
+    def enable_hardware_radio(self):
+        subprocess.Popen('netsh interface set interface "WiFi" enable', shell=True).communicate()
+
+    def disable_hardware_radio(self):
+        '''TODO: Provide powershelll privileges'''
+        subprocess.Popen('powershell.exe -noprofile "Start-Process -Verb RunAs -Wait powershell.exe -Args -noprofile; netsh interface set interface WiFi disable"',
+                         shell=True, stdin=subprocess.PIPE).communicate()
+        subprocess.Popen('netsh interface set interface "WiFi" disable', shell=True, stdin=subprocess.PIPE).communicate()
+        
+    def check_network_conn(self):
+        s = requests.Session()
+        s.get('192.168.1.1')
+        self.is_connected = True if s.status == 200 else False
+
     def list_bss_attrs(self, bss):
+        '''TODO:
+        Retrieve handshake algorithm'''
         for i in bss:
             bssid = i.bssid
             ssid  = i.ssid
             signal = i.signal
             freq = i.freq
             auth = 'Authorized' if i.auth == [] else 'Unauthorized'
-            akm = i.akm
-            print(f'{bssid}: {ssid}', signal, freq,  auth, akm)
+            # akm = i.akm
+            alg = i.alg
+            print(f'{bssid}: {ssid}', signal, freq, auth, alg)
 
     def get_networks_list(self, iface):
         '''
         Service Set (bss) attributes are bssid, ssid, freq, auth, akm, signal
 
-        TODO:
-         - Check if it gets connected automatically to each AP '''
+        TODO: Check if it gets connected automatically to each AP '''
         iface.scan()
-        time.sleep(0.5)
+        time.sleep(0.7)
         aps = iface.scan_results()
 
         self.list_bss_attrs(aps)
 
         return aps
     
+    def get_available_aps(self):
+        '''
+        TODO: Retrieve hardware_radio status'''
+        current_ap = subprocess.check_output(r'netsh WLAN show interfaces', encoding='utf-8')
+
+        if current_ap:
+            current_ap_status = current_ap.split('State')[1]
+            self.current_ap_status = current_ap_status.split('SSID')[0].split(':')[1].split('\n')[0].strip()
+
+            if self.current_ap_status == 'disconnected':
+                self.software_radio = current_ap.split('Software')[1].split()[0].strip()
+                self.current_ap_name = None
+
+            elif self.current_ap_status == 'connected':
+                self.software_radio = 'On'
+                self.current_ap_name = current_ap.split('SSID')[1].split(':')[1].split('\n')[0].strip()
+        
+            print(f'SW_Radio: {self.software_radio}')
+            print(f'AP_NAME: {self.current_ap_name}')
+            print(f'AP_STATUS: {self.current_ap_status}')
+
+        else:
+            self.software_radio = None
+            self.current_ap_status = None
+            self.current_ap_name = None
+
+
+    def georedundancy(self):
+        while True:
+            try:
+                self.get_available_aps()
+
+                if self.software_radio == 'On':
+                    if self.current_ap_status == 'disconnected':
+                        
+                        self.get_network_interfaces()
+                        if len(self.iface_a.name()) == 0:
+                            raise AssertionError('No network interfaces available')
+                        
+                        if self.current_ap_name is not None:
+
+                            if self.current_ap_name == creds.ap_b:
+                                target_ap = creds.ap_a
+                            elif self.current_ap_name == creds.ap_a:
+                                target_ap = creds.ap_b
+
+                        elif self.current_ap_name is None:
+                            target_ap = random.choice([creds.ap_a, creds.ap_b])
+
+                        self.connect_ap(self.iface_a, target_ap)
+
+                    elif self.current_ap_status is None:
+                        '''TODO: Enable Network Interface from Device Manager'''
+                        pass
+
+                    elif self.current_ap_status == 'connected':
+                        pass
+
+                    else:
+                        logging.error('Unknown AP or Interface error occurred')
+
+                elif self.software_radio == 'Off' or self.software_radio is None:
+                    '''TODO: Turn ON network interface'''
+                    pass
+
+            except Exception as e:
+                logging.error(f'Unknown {e}')
+                break
+
+            time.sleep(.7)
+
 
 wg = Geored_Wifi_Server()
-ifs = wg.get_network_interfaces()
-aps = wg.get_networks_list(ifs[0])
+wg.get_network_interfaces()
+wg.georedundancy()
