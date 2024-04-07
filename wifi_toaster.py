b62bfb3f96553da24ba7ac89e61b43b5cdd1594a
gerard <12766134+scripting-drafts@users.noreply.github.com>
2024-04-07 02:34:59 +0200

    release

--- wifi_toaster.py
+++ wifi_toaster.py
@@ -18,39 +18,28 @@ class Geored_Wifi_Server:
         self.wifi = pywifi.PyWiFi()
 
         self.logger = Logger().logging()
+        self.storage = Store()
 
-        self.cycle_len = .6
         self.threads_list = []
         self.urls = '8.8.8.8', '192.168.1.1', 'google.com'
-                
-    def heartbeats(self):
-        return
+        self.ping_results = []
 
-    def keepalive(self, url):
-            response = ping(url, verbose=False, timeout=1, size=1, count=1, interval=1.5)
-            self.logger.info(str(response).split('\n')[0]) # , count=2, interval=3=
+    def disable_software_radio(self, iface):
+        iface.disconnect()
+        time.sleep(.7)
+        assert iface.status() in\
+            [const.IFACE_DISCONNECTED, const.IFACE_INACTIVE]
+        
+    def enable_hardware_radio(self):
+        '''Enables hardware WiFi radio'''
+        subprocess.Popen('netsh interface set interface "WiFi" enable', shell=True).communicate()
 
-    def keepalived(self, mode):
-        if mode == 'start':
-            for url in self.urls:
-                t = threading.Thread(target=self.keepalive, daemon=True, args=(url,))
-                self.threads_list.append(t)
-                t.start()
-
-        elif mode == 'stop':
-            for t in self.threads_list:
-                t.join()
-
-    def get_network_interfaces(self):
-        '''
-        ifaces is a list of objects with the attribute ifaces.name
-        We assume ifaces will never be empty
-        '''
-        self.ifaces = self.wifi.interfaces()
-        self.iface_a = self.ifaces[0]
-
-        for u, w in enumerate(self.ifaces):
-            self.logger.info(f'iface {u}:', w.name())
+    def disable_hardware_radio(self):
+        '''Disables hardware WiFi radio'''
+        '''TODO: Provide powershelll privileges'''
+        subprocess.Popen('powershell.exe -noprofile "Start-Process -Verb RunAs -Wait powershell.exe -Args -noprofile; netsh interface set interface WiFi disable"',
+                         shell=True, stdin=subprocess.PIPE).communicate()
+        subprocess.Popen('netsh interface set interface "WiFi" disable', shell=True, stdin=subprocess.PIPE).communicate()
 
     def connect_ap(self, iface, ap):
         iface.remove_all_network_profiles()
@@ -64,25 +53,22 @@ class Geored_Wifi_Server:
         profile.key = pwd
         
         iface.connect(profile)
-        time.sleep(5)
+        time.sleep(30)
         assert iface.status() == const.IFACE_CONNECTED
 
-    def disable_software_radio(self, iface):
-        iface.disconnect()
-        time.sleep(.7)
-        assert iface.status() in\
-            [const.IFACE_DISCONNECTED, const.IFACE_INACTIVE]
-        
-    def enable_hardware_radio(self):
-        subprocess.Popen('netsh interface set interface "WiFi" enable', shell=True).communicate()
+    def get_network_interfaces(self):
+        '''
+        ifaces is a list of objects with the attribute ifaces.name
+        We assume ifaces will never be empty
+        '''
+        self.ifaces = self.wifi.interfaces()
+        self.iface_a = self.ifaces[0]
 
-    def disable_hardware_radio(self):
-        '''TODO: Provide powershelll privileges'''
-        subprocess.Popen('powershell.exe -noprofile "Start-Process -Verb RunAs -Wait powershell.exe -Args -noprofile; netsh interface set interface WiFi disable"',
-                         shell=True, stdin=subprocess.PIPE).communicate()
-        subprocess.Popen('netsh interface set interface "WiFi" disable', shell=True, stdin=subprocess.PIPE).communicate()
+        for u, w in enumerate(self.ifaces):
+            self.logger.info(f'iface {u}: {w.name()}' )
 
     def list_bss_attrs(self, bss):
+        '''List all bss attributes'''
         '''TODO:
         Retrieve handshake algorithm'''
         for i in bss:
@@ -96,88 +82,161 @@ class Geored_Wifi_Server:
             self.logger.info(f'{bssid}: {ssid}', signal, freq, auth, alg)
 
     def get_networks_list(self, iface):
-        '''
+        '''Fetch Available APs list
         Service Set (bss) attributes are bssid, ssid, freq, auth, akm, signal
-
         TODO: Check if it gets connected automatically to each AP '''
         iface.scan()
         time.sleep(0.7)
         aps = iface.scan_results()
 
-        self.list_bss_attrs(aps)
+        # self.logging.info(self.list_bss_attrs(aps))
+        self.logging.info(aps)
 
-        return aps
-    
-    def get_available_aps(self):
+        self.aps = aps
+
+    def get_network_status(self):
         '''
         TODO: Retrieve hardware_radio status'''
         current_ap = subprocess.check_output(r'netsh WLAN show interfaces', encoding='utf-8')
 
         if current_ap:
-            current_ap_status = current_ap.split('State')[1]
-            self.current_ap_status = current_ap_status.split('SSID')[0].split(':')[1].split('\n')[0].strip()
+            ap_current_status = current_ap.split('State')[1]
+            self.ap_current_status = ap_current_status.split('SSID')[0].split(':')[1].split('\n')[0].strip()
+            self.ap_disconnected_status = 'disconnected', 'disconnecting'
 
-            if self.current_ap_status == 'disconnected':
+            if self.ap_current_status in self.ap_disconnected_status:
+                self.hardware_radio = current_ap.split('Hardware')[1].split()[0].strip()
                 self.software_radio = current_ap.split('Software')[1].split()[0].strip()
                 self.current_ap_name = None
 
-            elif self.current_ap_status == 'connected':
+            elif self.ap_current_status == 'connected':
+                self.hardware_radio = 'On'
                 self.software_radio = 'On'
                 self.current_ap_name = current_ap.split('SSID')[1].split(':')[1].split('\n')[0].strip()
+
+            elif self.ap_current_status == 'discovering':
+                disco_wait = 5
+                self.logging.info('AP_STATUS: DISCOVERING, wait %d seconds', disco_wait)
+                time.sleep(disco_wait)
         
+            self.logger.info(f'HW_Radio: {self.hardware_radio}')
             self.logger.info(f'SW_Radio: {self.software_radio}')
             self.logger.info(f'AP_NAME: {self.current_ap_name}')
-            self.logger.info(f'AP_STATUS: {self.current_ap_status}')
+            self.logger.info(f'AP_STATUS: {self.ap_current_status}')
 
         else:
             self.software_radio = None
-            self.current_ap_status = None
+            self.ap_current_status = None
             self.current_ap_name = None
+
+    def keepalived(self, mode):
+        if mode == 'start':
+            for url in self.urls:
+                t = threading.Thread(target=self.keepalive_service, daemon=True, args=(url,))
+                self.threads_list.append(t)
+                t.start()
+
+        elif mode == 'stop':
+            for t in self.threads_list:
+                t.join()
+                self.ping_results = []
+
+    def keepalive_service(self, url):
+        try:
+            response = ping(url, verbose=False, timeout=1, size=1, count=1, interval=.8)
+            self.logger.info(str(response).split('\n')[0])
+
+            if response.success():
+                self.ping_results.append(True)
+
+            else:
+                self.ping_results.append(True)
+
+        except Exception:
+            self.logging.error(f'KeepAlive.d: NO_RESOLUTION for {url}')
+            self.ping_results.append(False)
+
+    def healthcheck(self):
+        self.get_network_status()
+
+        if self.software_radio == 'On':
+            if self.ap_current_status in self.ap_disconnected_status:
+                
+                self.get_network_interfaces()
+                if len(self.iface_a.name()) == 0:
+                    raise AssertionError('No network interfaces available')
+                
+                self.failover()
+
+            elif self.ap_current_status is None:
+                '''TODO: Enable Network Interface from Device Manager'''
+                pass
+
+            elif self.ap_current_status == 'connected':
+                pass
+
+            elif not any(self.ping_results):
+                self.failover()
+
+            else:
+                self.logger.error('Unknown AP or Interface error occurred')
+
+        elif self.software_radio == 'Off' or self.software_radio is None:
+            '''TODO: Turn ON network interface'''
+            pass
+
+    def failover(self):
+        if self.current_ap_name is not None:
+
+            if self.current_ap_name == creds.ap_b:
+                target_ap = creds.ap_a
+            elif self.current_ap_name == creds.ap_a:
+                target_ap = creds.ap_b
+
+        elif self.current_ap_name is None:
+            target_ap = random.choice([creds.ap_a, creds.ap_b])
+
+        self.connect_ap(self.iface_a, target_ap)
+        self.logger.info(f'Failover {self.current_ap_name} -> {target_ap}')
 
     def georedundancy(self):
         while True:
             try:
                 self.keepalived(mode='start')
-                self.get_available_aps()
-
-                if self.software_radio == 'On':
-                    if self.current_ap_status == 'disconnected':
-                        
-                        self.get_network_interfaces()
-                        if len(self.iface_a.name()) == 0:
-                            raise AssertionError('No network interfaces available')
-                        
-                        if self.current_ap_name is not None:
-
-                            if self.current_ap_name == creds.ap_b:
-                                target_ap = creds.ap_a
-                            elif self.current_ap_name == creds.ap_a:
-                                target_ap = creds.ap_b
-
-                        elif self.current_ap_name is None:
-                            target_ap = random.choice([creds.ap_a, creds.ap_b])
-
-                        self.connect_ap(self.iface_a, target_ap)
-
-                    elif self.current_ap_status is None:
-                        '''TODO: Enable Network Interface from Device Manager'''
-                        pass
-
-                    elif self.current_ap_status == 'connected':
-                        pass
-
-                    else:
-                        self.logger.error('Unknown AP or Interface error occurred')
-
-                elif self.software_radio == 'Off' or self.software_radio is None:
-                    '''TODO: Turn ON network interface'''
-                    pass
-
-                # time.sleep(self.cycle_len)
+                self.healthcheck()
                 self.keepalived(mode='stop')
 
             except Exception as e:
                 self.logger.error(f'Unknown {e}')
+
+    def test(self):
+        self.get_network_interfaces()
+        self.get_network_status()
+        if self.current_ap_name is not None:
+
+            if self.current_ap_name == creds.ap_b:
+                target_ap = creds.ap_a
+            elif self.current_ap_name == creds.ap_a:
+                target_ap = creds.ap_b
+
+        elif self.current_ap_name is None:
+            target_ap = random.choice([creds.ap_a, creds.ap_b])
+
+        self.connect_ap(self.iface_a, target_ap)
+        self.logger.info(f'Failover {self.current_ap_name} -> {target_ap}')
+        
+
+class Store:
+    def store(data):
+        '''Stores transfered data'''
+        filename = str(datetime.today().strftime("%d-%m-%Y_%H-%M-%S")) + '.txt'
+        f = open(f'ddbb/{filename}', 'a')
+
+        for line in data:
+            output_string = line + '\n'
+            f.write(output_string)
+
+        f.close()
 
 class Logger:
     '''Reusable logger class'''
@@ -195,19 +254,9 @@ class Logger:
             return '\u001b[0m'
 
     def logging(self):
-        # g0 = self.RGB()
-        # g1 = self.RGB(127, 255, 212)
-        # g2 = self.RGB(0, 0, 128)
-        # bold = "\033[1m"
-        # reset = "\033[0m"
-        logging.basicConfig(filename='logs_toast.log', encoding='UTF-8', level=logging.DEBUG, format='%(asctime)s [%(name)s] %(message)s')
+        logging.basicConfig(filename='logs_toast.log', filemode='w', encoding='UTF-8', level=logging.DEBUG, format='%(asctime)s [%(name)s] %(message)s')
         logger = logging.getLogger('WIFI_TOASTER')
         logger.setLevel(logging.DEBUG)
-        # ch = logging.StreamHandler()
-        # ch.setLevel(logging.DEBUG)
-        # formatter = logging.Formatter('%(asctime)s,%(msecs)03d {}{}[%(name)s]{}{} %(message)s'.format(bold, g1, g0, reset), '%H:%M:%S')
-        # ch.setFormatter(formatter)
-        # logger.addHandler(ch)
 
         return logger
 
